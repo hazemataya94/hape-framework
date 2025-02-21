0c49671b2fac64ca3b3ec5583924a05487bfe314
Hazem Ataya <hazem.ataya94@gmail.com>
2025-02-21 14:37:38 +0100

    crud generate: generate model columns

--- hape/hape_cli/controllers/crud_controller.py
+++ hape/hape_cli/controllers/crud_controller.py
@@ -10,25 +10,20 @@ class CrudController:
     def __init__(self, name, schema_json, schema_yaml):
         self.logger = Logging.get_logger('hape.hape_cli.controllers.crud_controller')
         schema = None
-        schema_and_name = None
         
         if schema_json:
             try:
-                schema_and_name = json.loads(schema_json)
+                schema = json.loads(schema_json)
             except json.JSONDecodeError as e:
                 self.logger.error(f"Invalid JSON schema: {schema_json}")
                 exit(1)
         elif schema_yaml:
             try:
-                schema_and_name = yaml.safe_load(schema_yaml)
+                schema = yaml.safe_load(schema_yaml)
             except yaml.YAMLError as e:
                 self.logger.error(f"Invalid YAML schema: {schema_yaml}")
         
-        if schema_and_name:
-            name = schema_and_name["name"]
-            schema = schema_and_name["schema"]
-        
-        self.crud = Crud(os.path.basename(os.getcwd()), name, schema)
+        self.crud = Crud(os.path.basename(os.getcwd()), schema)
         self.crud.validate()
     
     def generate(self):
