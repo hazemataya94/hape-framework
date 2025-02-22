fbea1567971b85920938db37cfbeefa56c64bb64
Hazem Ataya <hazem.ataya94@gmail.com>
2025-02-22 19:52:55 +0100

    Bump version: 0.2.97

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hape",
-    version="0.2.96",
+    version="0.2.97",
     packages=["hape"],
     include_package_data=True,
     install_requires=[
